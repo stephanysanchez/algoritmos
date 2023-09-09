52c3b0d5b0ceb9fa28ea16c2665452b4535b85ff
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-09-08 22:09:38 -0300

    Add files via upload
    Algoritmo Media de dois numeros

---
+++ medianum.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+
+    float A,B,X;
+	printf("Escreva o valor de A\n");
+	scanf("%f", &A);
+	
+	printf("Escreva o valor de B\n");
+	scanf("%f", &B);
+    X=(A+B)/2;
+	
+	printf("O valor da media desse numeros e %f \n", X);
+   
+	
+}
+
