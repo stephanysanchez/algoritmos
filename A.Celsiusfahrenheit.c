63d06bd657f79f55c48ecc85d6e82032d091fd67
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-09-13 15:24:27 -0300

    Add files via upload

---
+++ A.Celsiusfahrenheit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () 
+{
+float C,F; 
+printf("Digite o valor da temperatura em Celsius: \n");
+scanf("%f",&C);
+F=(9*C+160)/5;
+printf("O valor da temperatura em Fahrenheit e: %f \n",F);
+}
