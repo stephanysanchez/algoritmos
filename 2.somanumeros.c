444dc7559fa9bd7c730498fd33a7cd2ce72f227a
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-09-13 15:14:21 -0300

    Add files via upload

---
+++ 2.somanumeros.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+
+    int A,B,C;
+    printf("Escreva o valor de A: \n");
+    scanf("%d",&A);
+
+    printf("Escreva o valor de B: \n");
+    scanf("%d",&B);
+    C=A+B;
+    printf("O valor da soma dos valores e: %d \n",C);
+}
