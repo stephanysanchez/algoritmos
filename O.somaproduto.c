4a28d004dbcc69afa388ae6733b8f2edc9705dad
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-09-13 15:35:01 -0300

    Add files via upload

---
+++ O.somaproduto.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+int A,B,C,D,S,P;
+printf("Digite o valor de A: \n");
+scanf("%d",&A);
+printf("Digite o valor de B: \n");
+scanf("%d",&B);
+printf("Digite o valor de C: \n");
+scanf("%d",&C);
+printf("Digite o valor de D: \n");
+scanf("%d",&D);
+S=B+D;
+P=A*C;
+printf("O valor da soma e: %d \n",S);
+printf("O valor do produto e: %d \n",P);
+}
