4d7a840a41683d15e7470355a40a44c6e3c60a9c
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-09-13 15:42:07 -0300

    Add files via upload

---
+++ Y.antecessorsucessor .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+int N,S,A;
+printf("Digite o valor do numero: \n");
+scanf("%d",&N);
+A=N-1;
+printf("O antecessor e: %d \n",A);
+S=N+1;
+printf("O sucessor e: %d \n",S);
+}
