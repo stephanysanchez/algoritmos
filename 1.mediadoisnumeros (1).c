852002c4f0deb8f1d40f901d5e62407b6956c749
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-09-13 15:13:42 -0300

    Add files via upload

---
+++ 1.mediadoisnumeros (1).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+int A,B;
+float C;
+printf("Digite o valor de A: \n");
+scanf("%d",&A);
+printf("Digite o valor de B: \n");
+scanf("%d",&B);
+C=(A+B)/2;
+printf("O valor da media e: %f \n",C);
+}
