d27070dc5501af06217ee8311cd736ca4d88e315
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-09-13 15:41:39 -0300

    Add files via upload

---
+++ X.raiz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+int Radicando;
+float Raiz;
+printf("Digite o radicando: \n");
+scanf("%d",&Radicando);
+Raiz=sqrt(Radicando);
+printf("A raiz e: %f \n",Raiz);
+}
