831f1683837b6228ac2b4a449d837cc768b4e2a1
stephanysanchez <142893845+stephanysanchez@users.noreply.github.com>
2023-09-13 15:31:37 -0300

    Add files via upload

---
+++ J.quadradodiferenca.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+int A,B,C,D;
+printf("Digite o valor de A: \n");
+scanf("%d",&A);
+printf("Digite o valor de B: \n");
+scanf("%d",&B);
+C=A-B;
+printf("O valor da diferenca e: %d \n",C);
+D=C*C;
+printf("O valor do quadrado da diferenca e: %d \n",D);
+}
